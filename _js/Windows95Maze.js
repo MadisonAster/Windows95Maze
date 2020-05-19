e5a51b904512412eb24438965054f6ceced6bbfe
Madison Aster <info@MadisonAster.com>
2020-05-19 00:14:39 -0700

    cool walls 12

--- _js/Windows95Maze.js
+++ _js/Windows95Maze.js
@@ -835,8 +835,8 @@ class Windows95Maze{
         };
         
         if(this.EnableGlobe){
-            var CoolTexture = this.MazeGlobeTexture;
-            //var CoolTexture = this.MazeCoolWallList[10];
+            //var CoolTexture = this.MazeGlobeTexture;
+            var CoolTexture = this.MazeCoolWallList[10];
             this.MazeCoolWallsMaterial = new THREE.MeshBasicMaterial({map: CoolTexture});
             
             var MazeCoolWallsActor = new Actor(0,0,0);
